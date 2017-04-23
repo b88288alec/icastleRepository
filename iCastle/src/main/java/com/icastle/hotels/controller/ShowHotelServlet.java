49b87c258587c9588738c8b4ffe6547939e83efe
郭先河 <vuockt123@gmail.com>
2017-04-23 13:43:50 +0800

    修改ShowHotelServlet.java
    修改ShowHotelServlet.java,使得hotel.jsp可以顯示飯店資料

--- iCastle/src/main/java/com/icastle/hotels/controller/ShowHotelServlet.java
+++ iCastle/src/main/java/com/icastle/hotels/controller/ShowHotelServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.icastle.hotels.model.*;
 import com.icastle.rooms.model.RoomsDAO_interface;
 import com.icastle.rooms.model.RoomsJDBCDAO;
 import com.icastle.rooms.model.RoomsJNDIDAO;
@@ -53,6 +54,15 @@ public class ShowHotelServlet extends HttpServlet {
 		List<RoomsVO> roomsvo =  roomsdao.findRooms(hotelId, peopleNum, start, end);
 		request.setAttribute("rooms", roomsvo);
 		System.out.println("抓到"+roomsvo.size()+"筆符合條件的房間");
+		
+		//查詢hotel的資料
+		HotelService hotelserv = new HotelService();
+		HotelVO hotel = hotelserv.findByPrimaryKey(hotelId);
+		request.setAttribute("hotel", hotel);
+		
+		//查詢hotel info
+		
+		//查詢hotel photo
 		
 		//轉交給hotel.jsp
 		RequestDispatcher rd = request.getRequestDispatcher("hotel/hotel.jsp");
