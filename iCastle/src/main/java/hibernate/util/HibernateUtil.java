91c9d9f0af23c519d69322eae01916536c7c196a
莊凱翔 <shawn@>
2017-04-28 14:27:12 +0800

    新增HibernateUtil.java連線功能

---
+++ iCastle/src/main/java/hibernate/util/HibernateUtil.java
@@ -0,0 +1,35 @@
+/*
+ * This class not only produces the global org.hibernate.SessionFactory reference in its static initializer;
+ * it also hides the fact that it uses a static singleton
+*/
+
+package hibernate.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory;
+
+    static {
+    	// 註冊服務
+    	Configuration cfg = new Configuration().configure();
+    	ServiceRegistry registry = new StandardServiceRegistryBuilder().applySettings(cfg.getProperties()).build();
+        try {
+        	// 創建SessionFactory
+			sessionFactory = cfg.buildSessionFactory(registry);
+        } catch (Throwable ex) {
+        	// Make sure you log the exception, as it might be swallowed
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+}
