6f6b0572421023867d195b22175b6e087949314b
王德慧 <Student@192.168.34.108>
2017-04-28 19:02:21 +0800

    test測試過全部沒問題

--- iCastle/src/main/java/com/icastle/hotelInfo/modle/InfoDAO_interface.java
+++ iCastle/src/main/java/com/icastle/hotelInfo/modle/InfoDAO_interface.java
@@ -3,7 +3,7 @@ package com.icastle.hotelInfo.modle;
 import java.util.List;
 
 public interface InfoDAO_interface  {
-	public void insert(InfoVO infoVO);
-	public void updateHotelInfo(InfoVO infoVO);
-	public InfoVO findByHotelId(Integer hotelId);
+	public void insert(InfoVO infoVO); /*飯店註冊*/
+	public void updateHotelInfo(InfoVO infoVO); /*飯店頁面更新*/
+	public InfoVO findByHotelId(Integer hotelId); /*某飯店頁面時查詢*/
 }
