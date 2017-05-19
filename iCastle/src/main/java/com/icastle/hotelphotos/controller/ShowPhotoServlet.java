53e91b2afdd09175b1cdff0c1c23f4f9fe5f8f9c
郭先河 <vuockt123@gmail.com>
2017-05-19 12:13:50 +0800

    房型圖片改為動態,新增圖片

--- iCastle/src/main/java/com/icastle/hotelphotos/controller/ShowPhotoServlet.java
+++ iCastle/src/main/java/com/icastle/hotelphotos/controller/ShowPhotoServlet.java
@@ -2,6 +2,7 @@ package com.icastle.hotelphotos.controller;
 
 import java.io.*;
 import java.net.URLConnection;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -22,8 +23,9 @@ public class ShowPhotoServlet extends HttpServlet {
 
     /*
      *	將HotelPhotos的照片讀出並秀在網頁上
-     *  根據id查詢飯店                 http://localhost:8081/iCastle/ShowPhoto.do?id=4
+     *  根據id查詢飯店                  http://localhost:8081/iCastle/ShowPhoto.do?id=4
      *  查詢某家飯店的第一張照片  http://localhost:8081/iCastle/ShowPhoto.do?id=4&type=hotelid
+     *  查詢某房型第一張照片         http://localhost:8081/iCastle/ShowPhoto.do?id=4&type=roomtypeid
      */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -35,8 +37,15 @@ public class ShowPhotoServlet extends HttpServlet {
 		HotelPhotosVO photo = null;
 		if ("hotelid".equalsIgnoreCase(type))
 			photo = dao.findByHotelIdTop1(id);
-		else
+		else if ("roomtypeid".equalsIgnoreCase(type)){
+			List<HotelPhotosVO> photos = dao.findByRoomTypeId(id);
+			if (photos.size() != 0)
+				photo = dao.findByRoomTypeId(id).get(0);
+			else
+				photo = dao.findByPrimaryKey(5);
+		} else 
 			photo = dao.findByPrimaryKey(id);
+		
 		byte[] buffer = photo.getPhoto();
 		
 		//將二進制資料寫出到網頁
